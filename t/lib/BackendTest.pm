b68625afa813e241c81a20667d3caa0f82e1841b
Felipe Gasper <felipe@felipegasper.com>
2020-01-04 23:45:33 -0500

    commit - potential parallel

--- t/lib/BackendTest.pm
+++ t/lib/BackendTest.pm
@@ -17,7 +17,14 @@ sub run_modulinos {
 
     my @modulino_nss;
 
-    for my $path ( glob $wildcard_path ) {
+    my @paths = glob $wildcard_path;
+
+    _run_series(@paths);
+    # _run_parallel(@paths);
+}
+
+sub _run_series {
+    for my $path ( @_ ) {
         my @test_path = File::Spec->splitpath($path);
         my $filename = $test_path[-1];
 
@@ -32,5 +39,43 @@ sub run_modulinos {
         }
     }
 }
+
+#sub _run_parallel {
+#    use Test::SharedFork;
+#
+#    use MCE::Loop;
+#    MCE::Loop::init( {
+#        chunk_size => 1,
+#        max_workers => 6,
+#    } );
+#
+#    my @out = mce_loop {
+#        my $path = $_;
+#        my @test_path = File::Spec->splitpath($path);
+#        my $filename = $test_path[-1];
+#
+#        my $module_name_leaf = $filename;
+#        $module_name_leaf =~ s<\.t\z><> or die "weird modulino name: $module_name_leaf";
+#
+#        my $ns = "t::$module_name_leaf";
+#
+#        my $out = q<>;
+#
+#        my $Test = Test::Builder->new;
+#        $Test->output(\$out);
+#        $Test->failure_output(\$out);
+#
+#        subtest $ns => sub {
+#            require $path;
+#            $ns->runtests();
+#        };
+#
+#        MCE->gather($out);
+#    } @_;
+#
+#    print "# ------- DONE\n";
+#
+#    print $_ for @out;
+#}
 
 1;
